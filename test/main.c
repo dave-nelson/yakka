da7624bd3c077d53b334513ea10433d97792e563
Dave Nelson <urbanus@240gl.org>
2011-04-29 08:29:32 +1000

    Initial setup of yakka project: libtool library and unit tests.

---
+++ test/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <assert.h>
+#include "yakka/yakka.h"
+
+int main()
+{
+    yakka_func ();
+    assert (1);
+    return (0);
+}
---
+++ yakka/lib.c
@@ -0,0 +1,14 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * lib.c
+ * Copyright (C) Dave Nelson 2011 <dave.nelson@240gl.org>
+ * 
+ */
+
+#include <stdio.h>
+
+int yakka_func(void)
+{
+	printf("Hello world\n");
+	return (0);
+}
